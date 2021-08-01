d6777f016bb25f3eb8521a1c2cad8f89de1e5abc
Aabhas Gupta <aabhasgupta97@gmail.com>
2021-08-02 02:29:40 +0530

    Added create transactions and pending transactions page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-
 import { BlockViewComponent } from './components/block-view/block-view.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 import { SettingsComponent } from './pages/settings/settings.component';
+import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
+import { PendingTransactionsComponent } from './pages/pending-transactions/pending-transactions.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { SettingsComponent } from './pages/settings/settings.component';
     BlockchainViewerComponent,
     BlockViewComponent,
     TransactionsTableComponent,
-    SettingsComponent
+    SettingsComponent,
+    CreateTransactionComponent,
+    PendingTransactionsComponent
   ],
   imports: [
     BrowserModule,
